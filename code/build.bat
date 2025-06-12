3814e0111dc33053c59b41fc7e7d68598f3fcca6
Christian <64344406+cjxo@users.noreply.github.com>
2025-06-12 11:31:48 +0800

    I can DRAW!

--- code/build.bat
+++ code/build.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 if not exist ..\build mkdir ..\build
 pushd ..\build
-ml64 /nologo /Fesnake.exe /Flsnakeasmlisting.txt /Fmsnakelinkermap.txt /W3 /Zi ..\code\main.asm /link /nodefaultlib /subsystem:windows Kernel32.lib user32.lib gdi32.lib
+ml64 /nologo /Fesnake.exe /Flsnakeasmlisting.txt /Fmsnakelinkermap.txt /W3 /Zi ..\code\main.asm /link /incremental:no /nodefaultlib /subsystem:windows Kernel32.lib user32.lib gdi32.lib winmm.lib
 rem cl /nologo /Zi /Od /W4 /FAs /Famain.asm ..\code\main.c /link /nodefaultlib /subsystem:windows kernel32.lib user32.lib gdi32.lib
 popd
 
--- code/main.asm
+++ code/main.asm
@@ -16,15 +16,28 @@ SetProcessDPIAware proto
 TranslateMessage proto
 DispatchMessageA proto
 AdjustWindowRect proto
+timeBeginPeriod proto
+QueryPerformanceFrequency proto
+QueryPerformanceCounter proto
+Sleep proto
+VirtualAlloc proto
+GetDC proto
+StretchDIBits proto
 
 .const
 w32_class_name byte "snake_asm_window_class", 0
 w32_window_name byte "snake", 0
-w32_window_width dword 720
-w32_window_height dword 1280
+w32_window_width dword 1280
+w32_window_height dword 720
 
 .data?
-w32_window_handle qword ?
+WindowHandle qword ?
+PerformanceFreq qword ?
+MSPerFrame qword ?
+RPixels qword ?
+RBackBufferWidth dword ?
+RBackBufferHeight dword ?
+RBackBufferBPP dword ?
 
 .code
 WinMainCRTStartup proc
@@ -33,9 +46,21 @@ WinMainCRTStartup proc
     ; 8 more params for CreateWindowExA: 64 bytes
     ; 4 registers: 32 bytes
     ; 72 + 32 + 64 + 32 = 200 bytes
-    sub rsp, 200
+    sub rsp, 216
     
     call SetProcessDPIAware
+    
+    mov ecx, 1
+    call timeBeginPeriod
+    
+    lea rcx, PerformanceFreq
+    call QueryPerformanceFrequency
+
+    mov rax, 1000
+    xor rdx, rdx
+    mov rcx, 60
+    div rcx
+    mov qword ptr [MSPerFrame], rax
     
     mov ecx, 4
     call GetStockObject
@@ -114,30 +139,140 @@ WinMainCRTStartup proc
     mov qword ptr [rsp + 80], rax
     mov qword ptr [rsp + 88], 0
     call CreateWindowExA
-    mov w32_window_handle, rax
+    mov WindowHandle, rax
     
     mov rcx, rax
     mov edx, 5
     call ShowWindow
+
+    mov rcx, 0
+    mov rdx, 3686400
+    mov r8d, 12288
+    mov r9d, 4
+    call VirtualAlloc
+    mov qword ptr [RPixels], rax
+
+    mov dword ptr [RBackBufferWidth], 1280
+    mov dword ptr [RBackBufferHeight], 720
+    mov dword ptr [RBackBufferBPP], 4
     
+    ; at this point, we have
+    ; MSG struct located at (rsp + 64) -> (rsp + 112)
+    ; LARGE_INTEGER located at (rsp + 112) -> (rsp + 120) - the start performance count
+    ; LARGE_INTEGER located at (rsp + 120) -> (rsp + 128)
+    ; BITMAPINFO located at (rsp + 128) -> (rsp + 172)
+    
+    lea rcx, [rsp + 112]
+    call QueryPerformanceCounter
+
 EntryApp_Loop:
+    ; Collect Some User Input
 EntryApp_PeekMessage:
-    mov dword ptr [rsp + 32], 1    ; PM_REMOVE
+    mov dword ptr [rsp + 32], 1   ; PM_REMOVE
     xor r9, r9                    ; wMsgFilterMax = 0
     xor r8, r8                    ; wMsgFilterMin = 0
     xor rdx, rdx                  ; NULL hWnd
-    lea rcx, [rsp + 40]           ; MSG struct
+    lea rcx, [rsp + 64]           ; MSG struct
     call PeekMessageA
     test eax, eax
     jz EntryApp_DoWork
     
-    lea rcx, [rsp + 40]           ; MSG struct
+    lea rcx, [rsp + 64]
     call TranslateMessage
-    lea rcx, [rsp + 40]           ; MSG struct
+    lea rcx, [rsp + 64]
     call DispatchMessageA
     jmp EntryApp_PeekMessage
     
 EntryApp_DoWork:
+    ; Game Update And Render Work
+    mov ecx, 0
+    mov edx, 0
+    mov r8, RPixels
+    mov r9d, dword ptr [RBackBufferWidth]
+    ; is this necessary?
+    xor rax, rax
+    mov eax, dword ptr [RBackBufferBPP]
+    mul r9d
+    mov r10, rax
+loop_y:
+    mov r9, r8
+loop_x:
+    mov byte ptr [r9 + 0], cl
+    mov byte ptr [r9 + 1], dl
+    mov byte ptr [r9 + 2], 0
+    mov byte ptr [r9 + 3], 0ffh
+    mov eax, dword ptr [RBackBufferBPP]
+    add r9, rax
+loop_x_test:
+    inc edx
+    cmp edx, dword ptr [RBackBufferWidth] 
+    jnae loop_x
+
+    add r8, r10
+    xor edx, edx
+loop_y_test:
+    inc ecx
+    cmp ecx, dword ptr [RBackBufferHeight]
+    jnae loop_y
+    
+    ; update screen and clear
+    mov rcx, WindowHandle
+    call GetDC
+
+    mov rcx, rax
+    xor edx, edx
+    xor r8d, r8d
+    mov r9d, dword ptr [w32_window_width]
+    mov eax, dword ptr [w32_window_height]
+    mov dword ptr [rsp + 32], eax
+    mov dword ptr [rsp + 40], 0
+    mov dword ptr [rsp + 48], 0
+    mov eax, dword ptr [RBackBufferWidth]
+    mov dword ptr [rsp + 56], eax
+    mov eax, dword ptr [RBackBufferHeight]
+    mov dword ptr [rsp + 64], eax
+    mov rax, qword ptr [RPixels]
+    mov qword ptr [rsp + 72], rax
+
+    mov dword ptr [rsp + 128], 40
+    mov eax, dword ptr [RBackBufferWidth]
+    mov dword ptr [rsp + 132], eax
+    mov eax, dword ptr [RBackBufferHeight]
+    neg eax
+    mov dword ptr [rsp + 136], eax
+    mov word ptr [rsp + 140], 1
+    mov eax, dword ptr [RBackBufferBPP]
+    shl ax, 3
+    mov word ptr [rsp + 142], ax
+    mov dword ptr [rsp + 144], 0
+    mov dword ptr [rsp + 148], 0
+    mov dword ptr [rsp + 152], 0
+    mov dword ptr [rsp + 156], 0
+    mov dword ptr [rsp + 160], 0
+    mov dword ptr [rsp + 164], 0
+    lea rax, [rsp + 128]
+    mov qword ptr [rsp + 80], rax
+    mov dword ptr [rsp + 88], 0
+    mov dword ptr [rsp + 96], 00CC0020h
+    call StretchDIBits
+
+    ; Cap FPS
+    lea rcx, [rsp + 120]
+    call QueryPerformanceCounter
+    mov rax, qword ptr [rsp + 120]  ; end count
+    sub rax, qword ptr [rsp + 112]  ; end count - start count
+    mov rcx, 1000
+    mul rcx                         ; (end count - start count) * 1000ms
+    div qword ptr [PerformanceFreq] ; ((end count - start count) * 1000) / CountsPerSecond
+    cmp rax, qword ptr [MSPerFrame] 
+    jge EntryApp_NewLoop            ; rax >= MSPerFrame
+    mov rcx, [MSPerFrame]           ; if rax < MSPerFrame, then rcx = MSPerFrame
+    sub rcx, rax                    ; rcx = rcx - rax
+    call Sleep                      ; Sleep(rcx = MSPerFrame - rax as DWORD)
+
+EntryApp_NewLoop:
+    lea rcx, [rsp + 112]
+    call QueryPerformanceCounter
     jmp EntryApp_Loop
     
     xor rcx, rcx
